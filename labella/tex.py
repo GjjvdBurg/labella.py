3253b630b9ce737e52bcc33e37507ed477301441
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2016-02-25 22:11:16 -0500

    enable translation of accented characters

--- labella/tex.py
+++ labella/tex.py
@@ -1,8 +1,41 @@
 
-import errno
 import os
 import tempfile
 import subprocess
+import unicodedata
+
+def uni2tex(text):
+    # Courtesy of https://tex.stackexchange.com/q/23410
+    accents = {
+        0x0300: '`', 0x0301: "'", 0x0302: '^', 0x0308: '"',
+        0x030B: 'H', 0x0303: '~', 0x0327: 'c', 0x0328: 'k',
+        0x0304: '=', 0x0331: 'b', 0x0307: '.', 0x0323: 'd',
+        0x030A: 'r', 0x0306: 'u', 0x030C: 'v',
+        }
+    out = ""
+    txt = tuple(text)
+    i = 0
+    while i < len(txt):
+        char = text[i]
+        code = ord(char)
+
+        # combining marks
+        if unicodedata.category(char) in ("Mn", "Mc") and code in accents:
+            out += "\\%s{%s}" % (accents[code], txt[i+1])
+            i += 1
+        # precomposed characters
+        elif unicodedata.decomposition(char):
+            base, acc = unicodedata.decomposition(char).split()
+            acc = int(acc, 16)
+            base = int(base, 16)
+            if acc in accents:
+                out += "\\%s{%s}" % (accents[acc], chr(base))
+            else:
+                out += char
+        else:
+            out += char
+        i += 1
+    return out
 
 def build_latex_doc(text, fontsize='11pt'):
     tex = r"""
@@ -16,7 +49,7 @@ def build_latex_doc(text, fontsize='11pt'):
 \typeout{{LABELWIDTH: \the\lblwidth}}%%
 \typeout{{LABELHEIGHT: \the\lblheight}}%%
 \end{{document}}
-""".format(fontsize=fontsize, text=text)
+""".format(fontsize=fontsize, text=uni2tex(text))
     return tex
 
 def compile_latex_doc(tex, silent=True):
