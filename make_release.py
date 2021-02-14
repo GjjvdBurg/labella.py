0f46a785f96ad490b3887ebc8c53fe7d414006a9
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-02-14 14:12:23 +0000

    Update release script

--- make_release.py
+++ make_release.py
@@ -14,6 +14,14 @@ Date: 2019-07-23
 
 import colorama
 import os
+import sys
+import tempfile
+import webbrowser
+
+
+URLS = {
+    "CI": "https://github.com/GjjvdBurg/labella.py/actions",
+}
 
 
 def colored(msg, color=None, style=None):
@@ -50,6 +58,13 @@ def get_package_name():
             (l.strip() for l in fp if l.startswith("NAME = ")), None
         )
         return nameline.split("=")[-1].strip().strip('"')
+
+
+def get_package_version(pkgname):
+    ctx = {}
+    with open(f"{pkgname}/__version__.py", "r") as fp:
+        exec(fp.read(), ctx)
+    return ctx["__version__"]
 
 
 class Step:
@@ -96,16 +111,21 @@ class UpdateChangelog(Step):
         self.print_run("vi CHANGELOG.md")
 
 
+class UpdateReadme(Step):
+    def action(self, context):
+        self.instruct("Update readme if necessary (check for version update)")
+        self.print_run("vi README.md")
+
+
 class RunTests(Step):
     def action(self, context):
-        self.instruct("Run the unit tests")
-        self.print_run("make test")
+        self.do_cmd("make test")
 
 
 class BumpVersionPackage(Step):
     def action(self, context):
-        self.instruct(f"Update __version__.py with new version")
-        self.print_run(f"vi {context['pkgname']}/__version__.py")
+        self.instruct("Update __version__.py with new version")
+        self.do_cmd(f"vi {context['pkgname']}/__version__.py")
 
     def post(self, context):
         wait_for_enter()
@@ -113,10 +133,7 @@ class BumpVersionPackage(Step):
 
     def _get_version(self, context):
         # Get the version from the version file
-        about = {}
-        with open(f"{context['pkgname'].lower()}/__version__.py", "r") as fp:
-            exec(fp.read(), about)
-        return about["__version__"]
+        return get_package_version(context["pkgname"])
 
 
 class MakeClean(Step):
@@ -143,15 +160,15 @@ class PushToTestPyPI(Step):
 
 class InstallFromTestPyPI(Step):
     def action(self, context):
-        self.print_run("cd /tmp/")
-        self.print_cmd("rm -rf ./venv")
-        self.print_cmd("virtualenv ./venv")
-        self.print_cmd("cd ./venv")
-        self.print_cmd("source bin/activate")
-        self.print_cmd(
-            "pip install --index-url https://test.pypi.org/simple/ "
-            + f"--extra-index-url https://pypi.org/simple {context['pkgname']}=={context['version']}"
+        tmpvenv = tempfile.mkdtemp(prefix="p2r_venv_")
+        self.do_cmd(
+            f"python -m venv {tmpvenv} && source {tmpvenv}/bin/activate && "
+            "pip install --no-cache-dir --index-url "
+            "https://test.pypi.org/simple/ "
+            "--extra-index-url https://pypi.org/simple "
+            f"{context['pkgname']}=={context['version']}"
         )
+        context["tmpvenv"] = tmpvenv
 
 
 class TestPackage(Step):
@@ -159,13 +176,14 @@ class TestPackage(Step):
         self.instruct(
             f"Ensure that the following command gives version {context['version']}"
         )
-        self.print_run(f"p2r -h")
+        self.do_cmd(
+            f"source {context['tmpvenv']}/bin/activate && pip list | grep {context['pkgname']}"
+        )
 
 
-class DeactivateVenv(Step):
+class RemoveVenv(Step):
     def action(self, context):
-        self.print_run("deactivate")
-        self.instruct("Go back to the project directory")
+        self.do_cmd(f"rm -rf {context['tmpvenv']}")
 
 
 class GitTagVersion(Step):
@@ -179,6 +197,16 @@ class GitAdd(Step):
         self.print_run("git gui")
 
 
+class GitAddRelease(Step):
+    def action(self, context):
+        self.instruct("Add Changelog & Readme to git")
+        self.instruct(
+            f"Commit with title: {context['pkgname']} Release {context['version']}"
+        )
+        self.instruct("Embed changelog in body commit message")
+        self.print_run("git gui")
+
+
 class PushToPyPI(Step):
     def action(self, context):
         self.do_cmd("twine upload dist/*")
@@ -189,53 +217,50 @@ class PushToGitHub(Step):
         self.do_cmd("git push -u --tags origin master")
 
 
-class WaitForTravis(Step):
+class WaitForCI(Step):
     def action(self, context):
-        self.instruct(
-            "Wait for Travis to complete and verify that its successful"
-        )
+        webbrowser.open(URLS["CI"])
+        self.instruct("Wait for CI to complete and verify that its successful")
 
 
-class WaitForAppVeyor(Step):
-    def action(self, context):
-        self.instruct(
-            "Wait for AppVeyor to complete and verify that its successful"
-        )
-
-
-class WaitForRTD(Step):
-    def action(self, context):
-        self.instruct(
-            "Wait for ReadTheDocs to complete and verify that its successful"
-        )
-
-
-def main():
+def main(target=None):
     colorama.init()
     procedure = [
-        GitToMaster(),
-        GitAdd(),
-        PushToGitHub(),
-        BumpVersionPackage(),
-        UpdateChangelog(),
-        MakeClean(),
-        RunTests(),
-        MakeDist(),
-        PushToTestPyPI(),
-        InstallFromTestPyPI(),
-        TestPackage(),
-        DeactivateVenv(),
-        GitAdd(),
-        PushToPyPI(),
-        GitTagVersion(),
-        PushToGitHub(),
+        ("gittomaster", GitToMaster()),
+        ("gitadd1", GitAdd()),
+        ("clean1", MakeClean()),
+        ("runtests", RunTests()),
+        # trigger CI to run tests on all platforms
+        ("push1", PushToGitHub()),
+        ("ci1", WaitForCI()),
+        ("bumpversion", BumpVersionPackage()),
+        ("gitadd2", GitAdd()),
+        ("changelog", UpdateChangelog()),
+        ("readme", UpdateReadme()),
+        ("dist", MakeDist()),
+        ("testpypi", PushToTestPyPI()),
+        ("install", InstallFromTestPyPI()),
+        ("testpkg", TestPackage()),
+        ("remove_venv", RemoveVenv()),
+        ("addrelease", GitAddRelease()),
+        ("pypi", PushToPyPI()),
+        ("tagfinal", GitTagVersion()),
+        # triggers CI to build with cibw and push to PyPI
+        ("push3", PushToGitHub()),
+        ("ci3", WaitForCI()),
     ]
     context = {}
     context["pkgname"] = get_package_name()
-    for step in procedure:
+    context["version"] = get_package_version(context["pkgname"])
+    skip = True if target else False
+    for name, step in procedure:
+        if not name == target and skip:
+            continue
+        skip = False
         step.run(context)
     cprint("\nDone!", color="yellow", style="bright")
 
 
 if __name__ == "__main__":
-    main()
+    target = sys.argv[1] if len(sys.argv) > 1 else None
+    main(target=target)
